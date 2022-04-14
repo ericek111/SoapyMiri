f13a3a96ca1a15674384b7b21bd6afbb16860ec4
ericek111 <erik.brocko@letemsvetemapplem.eu>
2022-04-14 20:22:39 +0200

    Revert "mirisdr set flavour and reset on open"
    This reverts commit 2b698a97
    
    Signed-off-by: ericek111 <erik.brocko@letemsvetemapplem.eu>

--- Settings.cpp
+++ Settings.cpp
@@ -24,11 +24,6 @@ SoapyMiri::SoapyMiri(const SoapySDR::Kwargs &args) :
     if (mirisdr_open(&dev, deviceIdx) != 0) {
         throw std::runtime_error("Unable to open LibMiriSDR device.");
     }
-
-    // we only support SDRplay
-    mirisdr_set_hw_flavour(dev, MIRISDR_HW_SDRPLAY);
-
-    mirisdr_reset(dev);
 }
 
 SoapyMiri::~SoapyMiri(void) {
